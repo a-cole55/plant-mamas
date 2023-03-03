b3ea7ab84b5a34107a587aee7120c7109b393614
a-cole55 <arniqua_c@yahoo.com>
2023-03-03 12:19:49 -0600

    add plant results data to html as a list

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -1,17 +1,21 @@
+document.querySelector('#search').addEventListener('click', getPlant);
+
 function getPlant(){
-    // let userInput = document.querySelector('input').value;
-    // //checks if user input has a space//
-    // if (userInput.includes(' ')){
-    //   search = userInput.replace(/ /g, "%");
-    // } else {
-    //   search = userInput
-    // }
-    fetch(`https://perenual.com/api/species-list?page=1&key=sk-3rgd63fa93f5a5fd2121&q=monstera`)
+    let userInput = document.getElementById('searchPlant').value;
+    //checks if user input has a space//
+    if (userInput.includes(' ')){
+      search = userInput
+      // search = userInput.replace(/ /g, "%");
+    } else {
+      search = userInput
+    }
+    fetch(`https://perenual.com/api/species-list?page=1&key=sk-3rgd63fa93f5a5fd2121&q=${search}`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data.data);
+      const plantResults = data.data;
     //   let drinkData = data.drinks;
-    //   displayData(drinkData, i)
+      displayData(plantResults)
     })
     .catch(err => {
       console.log(`error ${err}`)
@@ -20,4 +24,22 @@ function getPlant(){
     //   }
     });
   };
-getPlant();
+
+  function displayData(plantResults){
+    var resultsList = document.getElementById("plantResults");
+
+    for(let i = 0; i < plantResults.length; i++){
+       //create new li element
+       var plantListItem = document.createElement("li");
+
+       //add text
+       plantListItem.innerText = plantResults[i].scientific_name;
+
+       //add new list element built in previous steps to unordered list
+       //called numberList
+       resultsList.appendChild(plantListItem);
+
+
+    }
+  }
+  //clear input and delete old appended items
